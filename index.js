14bee8cdca0f350abf508de1cfb77b4cc95424e2
Tayab Raza <tayabraza@gmail.com>
2022-04-26 16:11:53 +0100

    creating html for test

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 (function(){
     var elements = document.querySelectorAll('*');
+    function Gapjs(click, change){
+        this.click = click;
+        this.change = change;
+
+        for (var i = 0; i < elements.length; i++){
+            if ( elements[i].dataset.gap ){
+                console.log(elements[i].dataset.gap)
+            }
+        }
+    }
+    Gapjs();
 })();
+
